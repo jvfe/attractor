248143e764c5ffc9c1e36fc44d1c0f3fa143efe3
jvfe <jvfecav@gmail.com>
2021-02-19 15:13:50 -0300

    feat: Add first version with only Lorenz

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
+let [x, y, z] = [1, 1, 1];
+
+const [sigma, rho, beta] = [10, 28, 8 / 3];
+
+const pointPositions = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600, WEBGL);
 }
 
 function draw() {
-  background(220);
-  ellipse(50, 50, 80, 80);
+  background("#fafafa");
+
+  const dt = 0.01;
+  const dx = sigma * (y - x) * dt;
+  const dy = (x * (rho - z) - y) * dt;
+  const dz = (x * y - beta * z) * dt;
+
+  x = x + dx;
+  y = y + dy;
+  z = z + dz;
+
+  pointPositions.push(new p5.Vector(x, y, z));
+  orbitControl();
+
+  scale(5);
+  stroke(140, 123, 200, 0.3);
+  noFill();
+
+  beginShape();
+  for (const vector of pointPositions) {
+    vertex(vector.x, vector.y, vector.z);
+    strokeWeight(3);
+  }
+  endShape();
 }
