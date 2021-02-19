56ab22d357350f5418ce761ba0f9d8db4c7959ea
jvfe <jvfecav@gmail.com>
2021-02-19 15:21:56 -0300

    refactor: Move Lorenz to factory function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
-let [x, y, z] = [1, 1, 1];
+const Lorenz = () => {
+  let [x, y, z] = [1, 1, 1];
 
-const [sigma, rho, beta] = [10, 28, 8 / 3];
+  const [sigma, rho, beta] = [10, 28, 8 / 3];
 
-const pointPositions = [];
+  const pointPositions = [];
+
+  const dt = 0.01;
+
+  const draw = () => {
+    const dx = sigma * (y - x) * dt;
+    const dy = (x * (rho - z) - y) * dt;
+    const dz = (x * y - beta * z) * dt;
+
+    x += dx;
+    y += dy;
+    z += dz;
+
+    pointPositions.push(new p5.Vector(x, y, z));
+
+    beginShape();
+    for (const vector of pointPositions) {
+      vertex(vector.x, vector.y, vector.z);
+      strokeWeight(3);
+    }
+    endShape();
+  };
+
+  return { draw };
+};
+
+let attractor = Lorenz();
 
 function setup() {
   createCanvas(600, 600, WEBGL);
@@ -11,26 +38,11 @@ function setup() {
 function draw() {
   background("#fafafa");
 
-  const dt = 0.01;
-  const dx = sigma * (y - x) * dt;
-  const dy = (x * (rho - z) - y) * dt;
-  const dz = (x * y - beta * z) * dt;
-
-  x = x + dx;
-  y = y + dy;
-  z = z + dz;
-
-  pointPositions.push(new p5.Vector(x, y, z));
-  orbitControl();
+  orbitControl(1, 1, 0.2);
 
   scale(5);
   stroke(140, 123, 200, 0.3);
   noFill();
 
-  beginShape();
-  for (const vector of pointPositions) {
-    vertex(vector.x, vector.y, vector.z);
-    strokeWeight(3);
-  }
-  endShape();
+  attractor.draw();
 }
